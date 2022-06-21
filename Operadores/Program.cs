b8a7883e77400f6218b35201fd03d886e21c900f
Fernando Cardenas <fernando.cardenas@paradox-et.com>
2022-06-21 14:46:00 -0500

    Add project files.

---
+++ Operadores/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Operador de suma 
+            int x = 50;
+            int y = 40;
+
+            Console.WriteLine("El resultado es de: " + (x + y));
+            Console.WriteLine(" ");
+
+            //Sumar valores de una variable
+            int sum1 = 12 + 12;
+            int sum2 = 3;
+            int sum3 = 5;
+
+            Console.WriteLine("La suma de cantidades es: " + (sum1 + sum2 + sum3));
+        }
+    }
+}
